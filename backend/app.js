ae4134e32f77d66adf7debf69996003651cba107
billShoe <pi.jp.314@gmail.com>
2018-02-09 13:20:51 -0800

    Password exncryption and login authorization

--- backend/app.js
+++ backend/app.js
@@ -10,13 +10,56 @@ app.use(express.static('public'))
 app.use(bodyParser.json())
 app.use(cors())
 
+const authorization = function(req, res, next){
+  const token = req.query.authToken || req.body.authToken
+  if(token){
+    User.findOne({
+      where: {authToken: token}
+    }).then((user)=>{
+      if(user){
+        req.currentUser = user
+        next()
+      }else{
+        res.status(401)
+        res.json({message:'Authorization Token Invalid'})
+      }
+    })
+  }else{
+    res.status(401)
+    res.json({message: 'Authorization Token Required'})
+  }
+}
+
+
 app.get('/', (req, res) => {
   res.json({message: 'API Example App'})
 });
 
-app.get('/user', (req, res) => {
-  User.findAll().then((user) => {
-      res.json({user: user})
+app.get('/user',
+authorization ,
+(req, res) => {
+  res.json({user: req.currentUser})
+})
+
+app.post('/user', function(req, res){
+  User.create(
+    {
+      email: req.body.email,
+      name: req.body.name,
+      password: req.body.password,
+      zip: req.body.zip
+    }
+  ).then((user)=>{
+    res.json({
+      message: 'success',
+      user: user
+    })
+  }).catch((error)=>{
+    res.status(400)
+    res.json({
+      message: "Unable to create User",
+      errors: error.errors
+    })
   })
 })
 
